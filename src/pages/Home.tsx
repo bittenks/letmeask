2dc59d44ca79920403d27a12aaacf4e9589621d1
Guilherme <guilhermebittencourtcorrea@gmail.com>
2021-06-22 16:02:22 -0300

    aula 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,29 @@
 import ilustrationImg from "../assets/images/ilustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
 
 export function Home() {
   return (
     <div>
       <aside>
-        <img src={ilustrationImg} alt="" />
+        <img src={ilustrationImg} alt="Ilustração" />
+        <strong> Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
+      <main>
+        <div>
+          <img src={logoImg} alt="logo" />
+          <button>
+            <img src={googleIconImg} alt="google icon" />
+            Crie sua sala com o Google
+          </button>
+          <div>Ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <button type="submit">Entrar na sala</button>
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
