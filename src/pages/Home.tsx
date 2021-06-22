c007e8cede52d482a559261f9b4e8c5d0c2db63c
Guilherme <guilhermebittencourtcorrea@gmail.com>
2021-06-22 15:53:45 -0300

    aula 2

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import ilustrationImg from "../assets/images/ilustration.svg";
+
+export function Home() {
+  return (
+    <div>
+      <aside>
+        <img src={ilustrationImg} alt="" />
+      </aside>
+    </div>
+  );
+}
